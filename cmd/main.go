04c159c4aecea2bf37b030584972803949780bff
Leonardo Dias Ferreira <leonardodf95@gmail.com>
2024-08-09 12:08:04 -0300

    feat: alterado o cmd para usar o pacote cobra, para corrigir a entrada de comandos e flags e ficar mais legivel o código

--- cmd/main.go
+++ cmd/main.go
@@ -1,69 +1,71 @@
 package main
 
 import (
-	"flag"
 	"fmt"
-	"os"
-	"path/filepath"
 
 	"github.com/leonardodf95/tinygit/internal/tinygit"
+	"github.com/spf13/cobra"
+)
+
+var (
+	path string
 )
 
 func main() {
 
-	// Definindo as flags
-	directoryFlag := flag.String("d", "", "Diretório de trabalho")
+	rootCmd := cobra.Command{}
+	rootCmd.CompletionOptions.DisableDefaultCmd = true
+	rootCmd.AddCommand(Init(), Status(), Commit())
+	rootCmd.Execute()
+}
 
-	// Fazendo o parse das flags
-	flag.Parse()
-
-	// Verificando se há argumentos suficientes
-	if len(os.Args) < 2 {
-		fmt.Println("Uso: go run main.go <comando> [opções]")
-		return
-	}
-	command := os.Args[1]
-
-	// Determina o diretório usando a flag ou o argumento padrão
-	var path string
-	if *directoryFlag != "" {
-		path = filepath.Clean(*directoryFlag)
-	} else {
-
-		pathAbs, err := filepath.Abs(".")
-		if err != nil {
-			fmt.Println("Erro ao determinar o diretório de trabalho:", err)
-			return
-		}
-		path = pathAbs
+func Init() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "init",
+		Short: "Inicializa o controle de versão",
+		Run: func(cmd *cobra.Command, args []string) {
+			err := tinygit.InitControlVersion(path)
+			if err != nil {
+				fmt.Println("Erro ao inicializar controle de versão:", err)
+			}
+		},
 	}
 
-	switch command {
-	case "help":
-		fmt.Println("Comandos disponíveis:")
-		fmt.Println("	init - Inicializa o controle de versão")
-		fmt.Println("		Uso: init -d / --directory <diretório>")
-		fmt.Println("	status - Mostra o status das mudanças")
-		fmt.Println("		Uso: status -d / --directory <diretório>")
-		fmt.Println("	commit - Salva as mudanças no controle de versão")
-		fmt.Println("		Uso: commit -d / --directory <diretório>")
-		fmt.Println("	help - Mostra esta mensagem de ajuda")
-	case "init":
-		err := tinygit.InitControlVersion(path)
-		if err != nil {
-			fmt.Println("Erro ao inicializar controle de versão:", err)
-		}
-	case "status":
-		_, err := tinygit.StatusControlVersion(path)
-		if err != nil {
-			fmt.Println("Erro ao verificar status:", err)
-		}
-	case "commit":
-		err := tinygit.CommitControlVersion(path)
-		if err != nil {
-			fmt.Println("Erro ao realizar commit:", err)
-		}
-	default:
-		fmt.Println("Comando desconhecido:", command)
+	cmd.Flags().StringVarP(&path, "directory", "d", "", "Diretório de trabalho")
+
+	return cmd
+}
+
+func Status() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "status",
+		Short: "Mostra o status de alterações do diretório monitorado pelo controle de versão",
+		Run: func(cmd *cobra.Command, args []string) {
+			_, err := tinygit.StatusControlVersion(path)
+			if err != nil {
+				fmt.Println("Erro ao verificar status:", err)
+			}
+		},
 	}
+
+	cmd.Flags().StringVarP(&path, "directory", "d", "", "Diretório de trabalho")
+
+	return cmd
+}
+
+func Commit() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "commit",
+		Short: "Salva as mudanças no controle de versão",
+		Run: func(cmd *cobra.Command, args []string) {
+			err := tinygit.CommitControlVersion(path)
+			if err != nil {
+				fmt.Println("Erro ao realizar commit:", err)
+			}
+		},
+	}
+
+	cmd.Flags().StringVarP(&path, "directory", "d", "", "Diretório de trabalho")
+
+	return cmd
 }
